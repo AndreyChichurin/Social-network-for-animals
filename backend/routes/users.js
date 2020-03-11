979423f89ad268e504405ba364b89a272e5ee64e
AndreyChichurin <9632779@gmail.com>
2020-03-11 14:21:25 +0300

    add sort

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -4,19 +4,52 @@ const router = express.Router();
 const UserAdd = require('../models/userAd');
 
 router.post('/', async function (req, res, next) {
-  const users = await UserAdd.find({id: {$ne : req.body.currentUserId}})
+  const users = await UserAdd.find({ id: { $ne: req.body.currentUserId } })
   res.json(users)
 })
 
 router.post('/like', async function (req, res, next) {
   // const user = await UserAdd.find({id:req.body.id})
-  await UserAdd.updateOne({id:req.body.id}, {$push: {likedBy : req.body.currentUserId}})
+  await UserAdd.updateOne({ id: req.body.id }, { $push: { likedBy: req.body.currentUserId } })
 })
 
 router.post('/dislike', async function (req, res, next) {
   // const user = await UserAdd.find({id:req.body.id})
-  await UserAdd.updateOne({id:req.body.id}, {$push: {dislikedUsers : req.body.currentUserId}})
+  await UserAdd.updateOne({ id: req.body.id }, { $push: { dislikedUsers: req.body.currentUserId } })
 })
+
+router.post('/likedby', async function (req, res, next) {
+  // console.log(req.body.currentUserId)
+  const currentUser = await UserAdd.find({ id: req.body.currentUserId })
+  console.log(currentUser[0].id)
+  const users = [];
+  for (let i = 0; i < currentUser[0].likedBy.length; i++) {
+    // const user = await UserAdd.find({ id: currentUser[0].likedBy[i] })
+    const user = await UserAdd.find(
+      { $and: [
+        { id: currentUser[0].likedBy[i] }, 
+        { likedBy: {$nin:['5'] }}
+      ] })
+    users.push(user)
+  }
+  const userss = users.flat(1)
+  console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>', userss)
+  res.json(userss)
+})
+
+// router.post('/match', async function (req, res, next) {
+//   // console.log(req.body.currentUserId)
+//   const currentUser = await UserAdd.find({ id: req.body.currentUserId })
+//   // console.log(currentUser[0].id)
+//   const users = [];
+//   for (let i = 0; i < currentUser[0].likedBy.length; i++){
+//     const user = await UserAdd.find({ $and : [{id: currentUser[0].likedBy[i]}, {likedBy: currentUser[0].id}] })
+//     users.push(user)
+//   }
+//   const userss = users.flat(1)
+//   console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>', userss)
+//   res.json(userss)
+// })
 
 // router.post('/superlike', async function (req, res, next) {
 //   console.log(req.body)
@@ -26,7 +59,7 @@ router.post('/dislike', async function (req, res, next) {
 //   res.json(users)
 // })
 
-module.exports = router; 
+module.exports = router;
 
 // const userReq = req.body.user
 //   const passwordToSave = bcrypt.hashSync(userReq.password, bcrypt.genSaltSync(10))
--- peterapp/src/components/scenes/Account/Account.js
+++ peterapp/src/components/scenes/Account/Account.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect }  from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
 import { makeStyles } from '@material-ui/core/styles';
@@ -7,7 +7,7 @@ import Paper from '@material-ui/core/Paper';
 import ButtonBase from '@material-ui/core/ButtonBase';
 import pink from '@material-ui/core/colors/pink';
 import red from '@material-ui/core/colors/red';
-import IconButton from'@material-ui/core/IconButton';
+import IconButton from '@material-ui/core/IconButton';
 // import AddIcon from '@material-ui/icons/Add';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
@@ -19,131 +19,106 @@ import { NavLink } from 'react-router-dom'
 // import pink from '@material-ui/core/Pink'
 import AnnouncementForm from "../AnnouncementForm/AnnouncementForm"
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    flexGrow: 1,
-  },
-  paper: {
-    padding: theme.spacing(3),
-    
-    maxWidth: 550,
-    backgroundColor: pink[50]
-  },
-  image: {
-    width: 250,
-    height: 250,
-  },
-  img: {
-    margin: 'auto',
-    display: 'block',
-    maxWidth: '100%',
-    maxHeight: '100%',
-  },
-  button:{
-    margin: 'auto',
-    position: 'relative',
-    backgroundColor: red[300]
-  },
-  block:{
-    display: 'block',
-    margin: 'auto',
-    position: 'relative'
-  }
-}));
+import Slider from '../Likedbyslider/slidercomponent'
 
+// const useStyles = makeStyles(theme => ({
+//   root: {
+//     flexGrow: 1,
+//   },
+//   paper: {
+//     padding: theme.spacing(3),
+
+//     maxWidth: 550,
+//     backgroundColor: pink[50]
+//   },
+//   image: {
+//     width: 250,
+//     height: 250,
+//   },
+//   img: {
+//     margin: 'auto',
+//     display: 'block',
+//     maxWidth: '100%',
+//     maxHeight: '100%',
+//   },
+//   button: {
+//     margin: 'auto',
+//     position: 'relative',
+//     backgroundColor: red[300]
+//   },
+//   block: {
+//     display: 'block',
+//     margin: 'auto',
+//     position: 'relative'
+//   }
+// }));
 
 export default function PersonalPage() {
 
-  const classes = useStyles();
-
-  const [likedBy, setLikedBy] = useState();
-  const [superLikedBy, setSuperLikedBy] = useState();
-  const [like, setLike] = useState();
-  const [superLike, setSuperLike] = useState();
-
-  useEffect(() => {
-    const fetchUsers = async () => {
-      try {
-        const response = await axios.get('http://localhost:5000/api/users');
-        console.log(response.data)
-        // setData(response.data);
-      } catch (e) {
-        console.log(e);
-        // setData({ users: data.users, isFetching: false });
-      }
-    };
-    fetchUsers();
-  }, []);
-
   return (
-    <div className={classes.root}>
-
-
-
-{/* //       {<Paper className={classes.paper} backgro>
-
-//       <Paper className={classes.paper} backgro>
-//         Username {/* Тут по идее должен быть лоигн зарегистрировавшегося пользователя} */}
-
-{/* //         <Grid container spacing={2}>
-
-//           <Grid item>
-//             <ButtonBase className={classes.image}>
-//               <img className={classes.img} alt="complex" src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRb1rbKH9VCUTzbqNwyRaVVoz5CFto6NTy5EHBxwnAd9kc4oUe7" />
-//             </ButtonBase>
-//           </Grid>
-
-//           <Grid item xs={6} sm container >
-//             <Grid item xs container direction="column" spacing={3} >
-//               <Grid item xs> */}
-//                 {/* <ul>
-//                   <li>Name</li>
-//                   <li>LastName</li>
-//                   <li>Phone</li>
-//                   <li>Hobby</li>
-
-//                 </ul> */}
-{/* //                  <List component="nav" className='ListPersonalCard' aria-label="mailbox folders">
-//       <ListItem>
-//         Ф.И.О. <span>{" "}</span>
-//         <ListItemText primary="Андрей Андреевич Щукин" />
-//       </ListItem>
-//       <Divider />
-//       <ListItem>
-//         Город/Страна <span></span>
-//         <ListItemText primary="Петербург" />
-//       </ListItem>
-//     <Divider light />
-//       <ListItem>
-//         О Себе/Питомцах <span></span>
-//         <ListItemText primary="Мои питомцы ищет пару и хорошего хозяина или хозяйку, чтобы во время их утех не скучали." />
-//       </ListItem>
-//     </List>
-                
-//               </Grid>
-              
-//                   <IconButton aria-label="delete"className="addButton">
-//                     <GitHubIcon className="addButonFlex" /> 
-//                   </IconButton>              
-//             </Grid>             */}
-{/* //           </Grid>
-//         </Grid> */}
-
-
-{/* //       </Paper>
-//       <div className={classes.block}>
-//       <NavLink to='/announcement'>
-//         <Button className={classes.button} variant="contained" size="small" color="secondary" >   
-//            Добавить объявление
-//         </Button>
-//         </NavLink>
-//         <Button className={classes.button} variant="contained" size="small" color="primary" >
-//            Избранные
-//         </Button> */}
-
-        
-        </div>
-
-   
+    <div>
+      <Slider />
+    </div>
   );
 }
+
+      //   {<Paper className={classes.paper} backgro>
+
+      //   <Paper className={classes.paper} backgro>
+      //     Username { Тут по идее должен быть лоигн зарегистрировавшегося пользователя} 
+
+      //     <Grid container spacing={2}>
+
+      //       <Grid item>
+      //         <ButtonBase className={classes.image}>
+      //           <img className={classes.img} alt="complex" src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRb1rbKH9VCUTzbqNwyRaVVoz5CFto6NTy5EHBxwnAd9kc4oUe7" />
+      //         </ButtonBase>
+      //       </Grid>
+
+      //       <Grid item xs={6} sm container >
+      //         <Grid item xs container direction="column" spacing={3} >
+      //           <Grid item xs>
+      //             <ul>
+      //               <li>Name</li>
+      //               <li>LastName</li>
+      //               <li>Phone</li>
+      //               <li>Hobby</li>
+
+      //             </ul>
+      //              <List component="nav" className='ListPersonalCard' aria-label="mailbox folders">
+      //   <ListItem>
+      //     Ф.И.О. <span>{" "}</span>
+      //     <ListItemText primary="Андрей Андреевич Щукин" />
+      //   </ListItem>
+      //   <Divider />
+      //   <ListItem>
+      //     Город/Страна <span></span>
+      //     <ListItemText primary="Петербург" />
+      //   </ListItem>
+      // <Divider light />
+      //   <ListItem>
+      //     О Себе/Питомцах <span></span>
+      //     <ListItemText primary="Мои питомцы ищет пару и хорошего хозяина или хозяйку, чтобы во время их утех не скучали." />
+      //   </ListItem>
+      // </List>
+
+      //           </Grid>
+
+      //               <IconButton aria-label="delete"className="addButton">
+      //                 <GitHubIcon className="addButonFlex" /> 
+      //               </IconButton>              
+      //         </Grid>            
+      //       </Grid>
+      //     </Grid>
+
+
+      //   </Paper>
+      //   <div className={classes.block}>
+      //   <NavLink to='/announcement'>
+      //     <Button className={classes.button} variant="contained" size="small" color="secondary" >   
+      //        Добавить объявление
+      //     </Button>
+      //     </NavLink>
+      //     <Button className={classes.button} variant="contained" size="small" color="primary" >
+      //        Избранные
+      //     </Button> 
