266f55596d83ce0af68843bd2e28efecb4e9f63d
Maxim Baykevich <max.baikevich@gmail.com>
2020-03-12 21:15:10 +0300

    ok now\

--- peterapp/src/firebase/index.js
+++ peterapp/src/firebase/index.js
@@ -19,5 +19,5 @@ const storage = firebase.storage();
 const database = firebase.database()
 
 export { database, firebase,storage as default };
->>>>>>> e4f75fb89b987f11f16a7d2566e0f963e8058534
+
 
--- peterapp/src/index.js
+++ peterapp/src/index.js
@@ -12,16 +12,11 @@ import App from './App';
 
 
 
-<<<<<<< HEAD
-
-ReactDOM.render(<App />, document.getElementById('root'));
-=======
 ReactDOM.render(
 <Provider store={store}>
   <App />
 </Provider>, 
 document.getElementById('root'));
->>>>>>> e4f75fb89b987f11f16a7d2566e0f963e8058534
 
 serviceWorker.unregister();
 
