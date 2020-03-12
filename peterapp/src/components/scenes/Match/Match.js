a8031f31140e9fa5b64aa1f51e7a8e42edac5d06
AndreyChichurin <9632779@gmail.com>
2020-03-12 19:12:56 +0300

    add routes

--- peterapp/src/components/scenes/Match/Match.js
+++ peterapp/src/components/scenes/Match/Match.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
 import Matchslider from '../Matchslider/slidercomponent'
+import Navbar from "../Navbar/Navbar";
 
 export default function PersonalPage() {
 
   return (
     <div>
+      <Navbar />
       <Matchslider />
       
     </div>
--- peterapp/src/components/scenes/Matchslider/additionalComponents/actions.js
+++ peterapp/src/components/scenes/Matchslider/additionalComponents/actions.js
@@ -1,7 +1,7 @@
 import React from 'react'
 // import Rewind from './actionsSlider/rewind'
 import Dislike from './actionsSlider/dislike'
-import Like from './actionsSlider/like'
+// import Like from './actionsSlider/like'
 // import Superlike from './actionsSlider/superlike'
 
 const Actions =({person, modifySuperficialChoices}) => (
